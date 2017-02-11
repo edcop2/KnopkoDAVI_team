7dc4f958b0c0b34fad559a8d2e2724b4bb64cc11
edcop2 <edcop2@gmail.com>
2017-02-11 18:19:32 +0200

    Namek >_>

--- Discrod bot/Discrod bot/Program.cs
+++ Discrod bot/Discrod bot/Program.cs
@@ -11,18 +11,19 @@ namespace Discrod_bot
             string s = "";
             while (s != "EXIT")
             {
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://discordapp.com/api/webhooks/279675368674164736/bJ9tuXcpj7AzIJg5vETSttyza6_ly-RRbr0qRWbd4U97K5L1kFVla8TQjy5TfCNfNgdP");
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://discordapp.com/api/webhooks/279983798077095936/gSgY0d6ycQyVdO1CZdv8OecuPo3Lk11WRIKU9LOc3QxwkjKF3GdifyDqkUmT-DLL3IaY");
                 httpWebRequest.ContentType = "application/json";
                 httpWebRequest.Method = "POST";
                 s = Console.ReadLine();
                 using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 {
-                    string json = "{\"username\":\"4to-to_s_Gujhvoi\"," +
+                    string json = "{\"username\":\"Professor\"," +
                                   "\"avatar_url\":\"http://asu.kh.ua/wp-content/uploads/2012/01/Guzhva.jpg \"," +
                                   "\"content\":\"" + s + "\"}";
                     streamWriter.Write(json);
                     streamWriter.Flush();
                     streamWriter.Close();
+                    //namek
 
                 }
 
