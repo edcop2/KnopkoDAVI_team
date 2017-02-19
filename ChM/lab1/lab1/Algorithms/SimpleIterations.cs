fb835fa58ad48af3a5b8e13250eda166294ca493
edcop2 <edcop2@gmail.com>
2017-02-19 17:11:32 +0200

    Eh

---
+++ ChM/lab1/lab1/Algorithms/SimpleIterations.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lab1.Algorithms
+{
+    public class SimpleIterations
+    {
+        public double[][] A { get; set; }
+        public double[] B { get; set; }
+
+        public double[][] _B { get; set; }
+        public double[] _D { get; set; }
+
+        public List<string> Log { get; set; }
+
+        public SimpleIterations(int n)
+        {
+            A = new double[n][];
+            _B = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                A[i] = new double[n];
+                _B[i] = new double[n];
+            }
+            B = new double[n];
+            _D = new double[n];
+            Log = new List<string>();
+        }
+
+        private double[] Base(double[] x)
+        {
+            int n = x.Length;
+            double[] newX = new double[n];
+
+            for (int i = 0; i < n; i++)
+                newX[i] = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                newX[i] += B[i];
+                for (int j = 0; j < n; j++)
+                {
+                    if (j != i)
+                        newX[i] -= A[i][j] * x[j];
+                }
+                newX[i] /= A[i][i];
+            }
+
+            return newX;
+        }
+
+        private double FirstForm()
+        {
+            double sum = 0, sumt = 0;
+            int n = A.Length;
+            for (int i = 0; i < n; i++)
+            {
+                sumt = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                        sumt += Math.Abs(A[i][j] / A[i][i]);
+                }
+                if (sum < sumt)
+                    sum = sumt;
+            }
+            return sum;
+        }
+
+
+        public double[] Calculate(double eps)
+        {
+            Log.Clear();
+            int n = A.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                        _B[i][j] = A[i][j] / A[i][i];
+                    else
+                        _B[i][j] = 0;
+                }
+                _D[i] = B[i] / A[i][i];
+            }
+
+            double b1 = FirstForm();
+
+            if (b1 >= 1)
+            {
+                Log.Add("Данная система не сходится по этому алгоритму");
+                return null;
+            }
+
+            double[] res = _D;
+            double[] rest;
+            int k = 1;
+            string s = "Итерация №0\n";
+            s += string.Format("x1 = {0}; x2 = {1}; x3 = {2}; x4 = {3}\n********\n",
+                Math.Round(res[0], (eps % 1).ToString().Length - 2), Math.Round(res[1], (eps % 1).ToString().Length - 2), 
+                Math.Round(res[2], (eps % 1).ToString().Length-2), Math.Round(res[3], (eps % 1).ToString().Length - 2));
+            Log.Add(s);
+            do
+            {
+                s = "";
+                rest = res;
+                res = Base(res);
+                s += "Итерация №" + k + "\n";
+                s += string.Format("x1 = {0}; x2 = {1}; x3 = {2}; x4 = {3}\n********\n",
+                    Math.Round(res[0], (eps % 1).ToString().Length - 2), Math.Round(res[1], (eps % 1).ToString().Length - 2), 
+                    Math.Round(res[2], (eps % 1).ToString().Length - 2), Math.Round(res[3], (eps % 1).ToString().Length - 2));
+                Log.Add(s);
+                k++;
+            } while (Math.Abs(res.Sum() - rest.Sum()) > Math.Abs(1 - b1) / b1 * eps);
+
+            return res;
+        }
+    }
+
+}
---
+++ ChM/lab1/lab1/ViewModel/MainViewModel.cs
@@ -0,0 +1,200 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using lab1.Model;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+using lab1.Algorithms;
+using System.Windows;
+using System;
+
+namespace lab1.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+
+
+        private ObservableCollection<Matrix> _matrix;
+
+        public ObservableCollection<Matrix> Matrix
+        {
+            get
+            {
+                if (_matrix == null)
+                    _matrix = new ObservableCollection<Model.Matrix>() { new Model.Matrix(), new Model.Matrix(), new Model.Matrix(), new Model.Matrix() };
+                return _matrix;
+            }
+            set
+            {
+                _matrix = value;
+                RaisePropertyChanged("Matrix");
+            }
+        }
+
+
+
+        private RelayCommand _calculate;
+
+        public ICommand Calculate
+        {
+            get
+            {
+                if (_calculate == null)
+                    _calculate = new RelayCommand(ExecuteCalculate);
+                return _calculate;
+            }
+        }
+
+        public void ExecuteCalculate()
+        {
+            Text = "";
+            ResX = "";
+            double[][] a;
+            double[] b;
+            ToArrays(out a, out b);
+
+            int n = Matrix.Count;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i][i] == 0)
+                {
+                    Text = "Эл-ты на главной диагонали не должны быть нулевыми";
+                    return;
+                }
+            }
+
+            if (IsIt)
+            {
+
+                SimpleIterations si = new SimpleIterations(n);
+
+                si.A = a;
+                si.B = b;
+
+                double eps = TextEps.HasValue ? TextEps.Value : 0.001;
+
+                double[] res = si.Calculate(eps);
+
+                if (res == null)
+                {
+                    Text = si.Log[0];
+                    return;
+                }
+
+                for (int i = 0; i < si.Log.Count; i++)
+                {
+                    Text += si.Log[i];
+                }
+
+                for (int i = 0; i < n; i++)
+                    ResX += "x" + (i + 1) + " = " + Math.Round(res[i], (eps % 1).ToString().Length - 2) + '\n';
+            }
+            else
+            {
+
+            }
+
+
+        }
+
+        private void ToArrays(out double[][] a, out double[] b)
+        {
+            int n = Matrix.Count;
+            a = new double[n][];
+            b = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new double[i];
+                a[i] = Matrix[i].ToArray();
+                b[i] = Matrix[i].B;
+            }
+
+        }
+
+
+        private string _text;
+
+        public string Text
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_text))
+                    _text = "";
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                RaisePropertyChanged("Text");
+            }
+        }
+
+        private string _resX;
+
+        public string ResX
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_resX))
+                    _resX = "";
+                return _resX;
+            }
+            set
+            {
+                _resX = value;
+                RaisePropertyChanged("ResX");
+            }
+        }
+
+        private double? _epsText;
+
+        public double? TextEps
+        {
+            get
+            {
+                if (_epsText == null)
+                    _epsText = 0.001;
+                return _epsText;
+            }
+            set
+            {
+                if (value > 0)
+                    _epsText = value;
+                else
+                    _epsText = null;
+                RaisePropertyChanged("TextEps");
+            }
+        }
+
+        private bool _isIt=true;
+
+        public bool IsIt
+        {
+            get
+            {
+                return _isIt;
+            }
+            set
+            {
+                _isIt = value;
+                RaisePropertyChanged("IsIt");
+            }
+        }
+
+        private bool _isGaus;
+
+        public bool IsGaus
+        {
+            get
+            {
+                return _isGaus;
+            }
+            set
+            {
+                _isGaus = value;
+                RaisePropertyChanged("IsGaus");
+            }
+        }
+    }
+}
