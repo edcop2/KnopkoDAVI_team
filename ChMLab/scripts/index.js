6bfd51093bed5aef3b5ac1aa400dffe33e5b2955
edcop2 <edcop2@gmail.com>
2017-02-19 03:25:32 +0200

    Ok

--- ChMLab/scripts/index.js
+++ ChMLab/scripts/index.js
@@ -20,17 +20,6 @@ function showresult() {
             j++;
         }
     }
-    var s="";
-    var s2="";
-    for (i=0; i<4; i++){
-        s2+=r[i]+" ";
-        for (j=0; j<4; j++){
-            s+=a[i][j]+" ";
-        }
-        s+="; ";
-    }
-    console.log(s);
-    console.log(s2);
 
     var p=calc(0.001);
     console.log(p[0] + "  "+  p[1] + "  "+  p[2] + "  " + p[3]);
